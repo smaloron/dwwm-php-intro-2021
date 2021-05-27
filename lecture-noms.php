5ddd00750071741a7a0e1c0141dc39d4db7ca0cc
Sebastien Maloron <contact@smaloron.eu>
2021-05-27 12:10:53 +0200

    lecture de fichier texte

--- lecture-noms.php
+++ lecture-noms.php
@@ -5,6 +5,26 @@
     // Transforme la chaine de caractère en tableau
     $list = explode("\n", $data);
 
+    // Le formulaire a-til été envoyé
+    $isPosted = filter_has_var(INPUT_POST, "submit");
+var_dump($_POST);
+    if($isPosted){
+        // Récupération de la saisie
+        $name = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_STRING);
+var_dump($name);
+        // Insertion dans la liste si la saisie n'est pas vide
+        if(! empty($name)){
+            // Ajout au tableau
+            array_push($list, $name);
+            // transformation du tableau en chaine de caractère
+            $listAsString = implode("\n", $list);
+
+            // Enregistrement dans le fichier text
+            file_put_contents("data/noms.txt", $listAsString);
+
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -13,10 +33,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leture d'un fichier texte</title>
+    <title>Lecture d'un fichier texte</title>
 </head>
 <body>
     <?php include "navigation.php" ?>
+
+    <form method="post">
+        <input type="text" name="nom" placeholder="votre nom">
+        <button type="submit" name="submit">Valider</button>
+    </form>
     
     <!-- Affichage de la liste des noms -->
     <ul>
